3dd40016a537da818ea0cadcf331b7c831cc1eea
Fioringo <shtuk.andrey@gmail.com>
2019-01-04 04:30:27 -0500

    eh formatted a little bit

--- Assignment 2/myEvery.js
+++ Assignment 2/myEvery.js
@@ -6,8 +6,8 @@
 
 Array.prototype.myEvery = function (callback, condition) {
     var i;
-    for(i = 0; i < this.length; i++){
-        if(!callback.call(condition, this[i], i, this))
+    for (i = 0; i < this.length; i++){
+        if (!callback.call(condition, this[i], i, this))
             return false;
     }
     return true;

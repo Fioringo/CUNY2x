8aad6a28e4a9e9b9edd41916639139124f5c35c0
Fioringo <shtuk.andrey@gmail.com>
2019-01-08 04:01:28 -0500

    renaming folders fun

---
+++ Assignment 3/cantTouchThis.js
@@ -0,0 +1,8 @@
+// CUNY2x
+// Assignment 3 - Don't touch button
+// Author: Andrey Shtukenberg
+// Don't touch button javascript
+
+function noTouch() {
+    alert("Hey, I told you not to hover over me!");
+}
---
+++ Assignment 4/decrementer/src/App.js
@@ -0,0 +1,46 @@
+/*
+  CUNY2x
+  Assignment 4
+  Author: Andrey Shtukenberg
+  Decrementer App.js file, with all of the required functionality.
+*/
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+class Decrementer extends Component {
+
+  counter = 0;
+
+  constructor(props){
+    super(props);
+    this.state = {
+      state: this.state,
+    }
+  }
+
+  decrement(){
+    if(this.counter >= this.props.number){
+      alert("Can't go below 0!");
+    } else {
+      this.counter += 1;
+      this.setState({state: this.state});
+    }
+  }
+
+  render() {
+    return(
+        <div className="main">
+          <div className="number">{String(this.props.number - this.counter)}</div>
+          <button className="decrementerBtn" onClick={() => this.decrement()}>Subtract 1</button>
+        </div>
+      )
+  };
+}
+
+Decrementer.propTypes = {
+  number: PropTypes.number,
+}
+
+export default Decrementer;

bcd4f06cb53f168ae494458fd60868f42bceb396
Fioringo <shtuk.andrey@gmail.com>
2019-01-08 00:39:11 -0500

    Added comments and redid some css. The page should look *fancy*

--- Assignment 3/contact-card/src/App.js
+++ Assignment 3/contact-card/src/App.js
@@ -1,9 +1,16 @@
+/*
+  CUNY2x
+  Assignment 4
+  Author: Andrey Shtukenberg
+  Contact Cards App.js file, with all of the required functions.
+*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
-class ContactCard extends React.Component {
-  
+class ContactCard extends Component {
+
   render() {
     let wpn = this.props.work_phone_number;
     let phoneto = "tel:" + this.props.mobile_number;
@@ -12,7 +19,7 @@ class ContactCard extends React.Component {
     if(wpn !== ""){
       return(
           <div className="ContactCard">
-            <div className="Name"> {this.props.name} </div>
+            <div className="Name"><strong> {this.props.name} </strong></div>
             <a
             href={phoneto}
             > {this.props.mobile_number} </a><br/>
@@ -27,7 +34,8 @@ class ContactCard extends React.Component {
     } else {
       return(
           <div className="ContactCard">
-            <div className="Name"> {this.props.name} </div>
+            <div className="Centerer"> </div>
+            <div className="Name"><strong> {this.props.name}</strong> </div>
             <a
             href={phoneto}
             > {this.props.mobile_number} </a><br/>
@@ -40,13 +48,20 @@ class ContactCard extends React.Component {
   }
 }
 
-class Cards extends React.Component {
+ContactCard.PropTypes = {
+  work_phone_number: PropTypes.string,
+  mobile_number: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+};
+
+class Cards extends Component {
   render() {
     return(
       <div className="Cards">
-        <ContactCard name="Poppy Harlow" mobile_number="(420)399-6669" work_phone_number="" email="meme-review@wsj.com"/>
-        <ContactCard name="Gloria Borger" mobile_number="(420)399-6669" work_phone_number="" email="meme-review@wsj.com"/>
-        <ContactCard name="Pew Die Pie" mobile_number="(399)420-1234" work_phone_number="(399)399-3999" email="meme-review@wsj.com"/>
+          <ContactCard name="Poppy Harlow" mobile_number="(202)555-0191" work_phone_number="(501)555-0155" email="meme-review@wsj.com"/>
+          <ContactCard name="Gloria Borger" mobile_number="(420)399-6669" work_phone_number="" email="meme-review@wsj.com"/>
+          <ContactCard name="Pew Die Pie" mobile_number="(399)420-1234" work_phone_number="(399)399-3999" email="meme-review@wsj.com"/>
       </div>
     );
   }
--- Assignment 3/contact-card/src/index.js
+++ Assignment 3/contact-card/src/index.js
@@ -1,3 +1,10 @@
+/*
+  CUNY2x
+  Assignment 4
+  Author: Andrey Shtukenberg
+  Contact Cards index.js file
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';

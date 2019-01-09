44da199161f983a0c4ae8f59a44630d16cffe84a
Fioringo <shtuk.andrey@gmail.com>
2019-01-09 00:24:59 -0500

    updated initial name in the form

--- React/form-edit/src/index.js
+++ React/form-edit/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App firstName = "Parakram" lastName = "Basnet" />, document.getElementById('root'));
+ReactDOM.render(<App firstName = "Andrey" lastName = "Shtukenberg" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
